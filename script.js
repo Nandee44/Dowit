02bae50fc2a5f8ca577f9be6ae7d97eb27e1043e
Kawtar <kawtarziadi6@gmail.com>
2025-05-23 18:05:43 +0100

    added modify task function + fixed some issues

--- script.js
+++ script.js
@@ -30,6 +30,7 @@ newtext.addEventListener("keydown", e => {
         textt = "";
         document.getElementById("newtext").value = "";
 
+        //mark checked or unchecked
         let checked = false;
         const img = document.getElementById(`circle-${numt}`);
         img.addEventListener("click", e => {
@@ -46,6 +47,67 @@ newtext.addEventListener("keydown", e => {
         if (newtask.id == "task-1") {
             newtask.style.borderTop = "1px solid #a3b18a";
         }
+
+        // modify task
+        newtask.addEventListener("dblclick", ee => {
+            const taskp = document.getElementById(`text-${numt}`);
+            //insert input area
+            let teext = newtask.innerText;
+            if (checked == false) {
+                newtask.innerHTML = `
+                    <img src="ellipse-outline.svg" alt="check" class="circle" id="circle-${numt}">
+                    <input type="text" class="outfit inpp" id="newtext-${numt}">
+                `;
+            }
+            else {
+                newtask.innerHTML = `
+                    <img src="checkmark-circle.svg" alt="check" class="circle" id="circle-${numt}">
+                    <input type="text" class="outfit inpp" id="newtext-${numt}">
+                `;
+            }
+            const inpp = document.getElementById(`newtext-${numt}`);
+            inpp.value = teext;
+
+            let someinput = document.getElementById(`newtext-${numt}`);
+            someinput.addEventListener("keydown", eee => {
+                if (eee.key == "Enter" && inpp.value != "") {
+                    //finish update 
+                    if (checked == false) {
+                        newtask.innerHTML = `
+                        <img src="ellipse-outline.svg" alt="check" class="circle" id="circle-${numt}">
+                        <div class="txt outfit"><p id="text-${numt}"></p></div>
+                    `;
+                    }
+                    else {
+                        newtask.innerHTML = `
+                        <img src="checkmark-circle.svg" alt="check" class="circle" id="circle-${numt}">
+                        <div class="txt outfit"><p id="text-${numt}"></p></div>
+                    `;
+                    }
+                    //again check or uncheck
+                    const img = document.getElementById(`circle-${numt}`);
+                    img.addEventListener("click", e => {
+                        if (checked == false) {
+                            img.src = "checkmark-circle.svg";
+                            checked = true;
+                        }
+                        else {
+                            img.src="ellipse-outline.svg";
+                            checked = false;
+                        }
+                    });
+                    
+                    document.getElementById(`text-${numt}`).innerText = inpp.value;
+                    teext = "";
+                }
+                if (eee.key == "Backspace" || eee.key == "Enter") {
+                    return
+                }
+                teext += eee.key;
+
+            });
+            
+        });
     }
             
 });
